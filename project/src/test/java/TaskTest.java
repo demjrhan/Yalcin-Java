76036565616797264268002508e753e3695648fb
demjrhan <demirhanylcn@gmail.com>
2025-10-17 22:33:29 +0200

    Added some edge cases for tests, task is finished

--- project/src/test/java/TaskTest.java
+++ project/src/test/java/TaskTest.java
@@ -6,19 +6,29 @@ import java.util.List;
 public class TaskTest {
     @Test
     public void greaterThan7() {
+        Assert.assertEquals(TaskLogic.greetIfGreater7(9), "Hello");
         Assert.assertEquals(TaskLogic.greetIfGreater7(8), "Hello");
         Assert.assertEquals(TaskLogic.greetIfGreater7(7), "");
+        Assert.assertEquals(TaskLogic.greetIfGreater7(6), "");
+        Assert.assertEquals(TaskLogic.greetIfGreater7(5), "");
         Assert.assertEquals(TaskLogic.greetIfGreater7(-5), "");
         Assert.assertEquals(TaskLogic.greetIfGreater7(-55), "");
         Assert.assertEquals(TaskLogic.greetIfGreater7(Integer.MIN_VALUE), "");
+        Assert.assertEquals(TaskLogic.greetIfGreater7(Integer.MAX_VALUE), "Hello");
     }
 
     @Test
     public void greetJohn() {
         Assert.assertEquals(TaskLogic.greetName("John"), "Hello, John");
         Assert.assertEquals(TaskLogic.greetName("john"), "Hello, John");
+        Assert.assertEquals(TaskLogic.greetName("john "), "Hello, John");
+        Assert.assertEquals(TaskLogic.greetName(" john "), "Hello, John");
+        Assert.assertEquals(TaskLogic.greetName(" john"), "Hello, John");
         Assert.assertEquals(TaskLogic.greetName("Mary"), "There is no such name");
         Assert.assertEquals(TaskLogic.greetName(null), "There is no such name");
+        Assert.assertEquals(TaskLogic.greetName(""), "There is no such name");
+        Assert.assertEquals(TaskLogic.greetName("\n"), "There is no such name");
+        Assert.assertEquals(TaskLogic.greetName("\\s"), "There is no such name");
     }
 
     @Test
@@ -28,6 +38,7 @@ public class TaskTest {
         Assert.assertEquals(TaskLogic.multiplesOf3(null), List.of());
         Assert.assertEquals(TaskLogic.multiplesOf3(new int[]{1, 2, 4, 5, 7}), List.of());
         Assert.assertEquals(TaskLogic.multiplesOf3(new int[]{0, -3, 3}), List.of(0, -3, 3));
+        Assert.assertEquals(TaskLogic.multiplesOf3(new int[]{Integer.MIN_VALUE, -3, 3}), List.of(-3, 3));
     }
 
     @Test
@@ -35,6 +46,9 @@ public class TaskTest {
         Assert.assertEquals(Task.isBalanced("[]()()"), "Given bracket sequence is correct.");
         Assert.assertEquals(Task.isBalanced("[)"), "Given bracket sequence is incorrect.");
         Assert.assertEquals(Task.isBalanced("[(])"), "Given bracket sequence is incorrect.");
+        Assert.assertEquals(Task.isBalanced("()(){"), "Given bracket sequence is incorrect.");
+        Assert.assertEquals(Task.isBalanced("{()}"), "Given bracket sequence is incorrect.");
+        Assert.assertEquals(Task.isBalanced("[(  ])"), "Given bracket sequence is incorrect.");
         Assert.assertEquals(Task.isBalanced("[((())()(()) )]".replace(" ", "")),
                 "Given bracket sequence is correct.");
         Assert.assertEquals(Task.isBalanced("   "),
