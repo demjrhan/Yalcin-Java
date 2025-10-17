ced5941883f54aece2c6435a001cb719c16b3532
demjrhan <demirhanylcn@gmail.com>
2025-10-17 16:04:17 +0200

    first commit

---
+++ src/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    public static void main(String[] args) {
+
+
+        System.out.println("Given bracket sequence: [((())()(())]] \n" +
+                "Can this sequence be considered correct? \n" +
+                "If the answer to the previous question is “no”, then what needs to be changed in it to make it correct? ");
+
+    }}
